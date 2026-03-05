172666e63c2adad54c9c776d1976398ddead2b9b
OGA-SA <agustina.sb24@gmail.com>
2026-03-05 14:17:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -314,6 +314,7 @@ page.drawText("Siniestro:", {
 // CAMPO SIN BORDE
 const siniestroField = form.createTextField("siniestro");
 siniestroField.setText(data.siniestro1 || "");
+siniestroField.setTextColor(rgb(0,0,0));
 siniestroField.addToPage(page,{
   x: rightColX + 50,
   y: fila1Y - 5,
@@ -333,6 +334,7 @@ page.drawText("Año:", {
 // CAMPO AÑO
 const anioField = form.createTextField("anio");
 anioField.setText(data.siniestro2 || "");
+anioField.setTextColor(rgb(0,0,0));
 anioField.addToPage(page,{
   x: rightColX + 125,
   y: fila1Y - 5,
@@ -370,6 +372,7 @@ page.drawText("¿Dificultad visual?:", {
 // CAMPO SIN BORDE
 const dificultadVisualField = form.createTextField("dificultadVisual");
 dificultadVisualField.setText(data.dificultadVisual || "");
+dificultadVisualField.setTextColor(rgb(0,0,0));
 
 dificultadVisualField.addToPage(page,{
   x: rightColX + 105,
@@ -594,6 +597,7 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto", PORT);
 });
+
 
 
 
